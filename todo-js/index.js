d0216b07effe2ee264612d1cc803e73fd084bc94
sajiki09 <gaz98205@gmail.com>
2021-12-12 23:15:34 +0900

    Add website file

--- todo-js/index.js
+++ todo-js/index.js
@@ -1,9 +1,4 @@
-import "./styles.css";
+let lines = require("fs").readFileSync("/dev/stdin", "utf8").split("\n");
+let userName = lines[0];
+console.log(`Hello ${userName}`);
 
-const onClickAdd = () => {
-  alert();
-};
-
-document
-  .getElementById("add-button")
-  .addEventListener("click", () => onClickAdd());
